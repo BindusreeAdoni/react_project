2cfebd55f486c0d528c18a6cf782eb83bfd62685
Bindu Sree <bindusreeadoni@gmail.com>
2025-08-05 21:25:02 +0530

    api correctly added

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
-const API_KEY = "15e8c45c"
-const BASE_URL =  "http://www.omdbapi.com/?i=tt3896198&apikey=15e8c45c"
+const API_KEY = "c8f00a0c22a814e49257b23190bf0004"
+const BASE_URL =  "https://api.themoviedb.org/3"
 
 
 export const getPopularMovies = async () => {
-    const response = await fetch(`${BASE_URL}/movie/popular?api-key=${API_KEY}`);
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
     const data = await response.json()
     return data.results
 
@@ -11,7 +11,7 @@ export const getPopularMovies = async () => {
 
 
 export const searchMovies = async (query) => {
-    const response = await fetch(`${BASE_URL}/search/movie?api-key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
     const data = await response.json()
     return data.results
 
